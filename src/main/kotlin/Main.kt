4d203b73d06f698487727a512a3079917f2f9744
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-03-09 08:56:27 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,47 @@
+fun main() {
+println(text("subsatnce","workload"))
+
+    var eds=("cynthia")
+    println(call("cyntia",20))
+    println(bob(12).equals(24))
+    println(numbers)
+
+
+}
+
+//Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length.
+fun text (name:String, word:String):String {
+    var name = "substance"
+    var word = "workload"
+    return (word)
+    println()
+}
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements
+
+    fun bob(numbers:Int,) {
+var c= arrayOf(45,12,67,908)
+    }
+//Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+fun call(name:String, age:Int){
+    var eds=("hi my name is $name and i am $age years old")
+}
+//Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+
+data class private constructor(val num1:Int, val num2:Int, val nume3:Int, val num4:Int){
+    var numbers=(12)
+
+
+
+}
+
+
+
+
